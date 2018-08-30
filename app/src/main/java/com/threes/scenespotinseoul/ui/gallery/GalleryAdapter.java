4fbfbd473eb7adc239fd6819a13b79b57bccfbb5
Unknown <gudxoiv@naver.com>
2018-08-30 22:17:16 +0900

    갤러리 기능 완료

---
+++ app/src/main/java/com/threes/scenespotinseoul/ui/gallery/GalleryAdapter.java
@@ -0,0 +1,84 @@
+package com.threes.scenespotinseoul.ui.gallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;;
+import com.threes.scenespotinseoul.R;
+import com.threes.scenespotinseoul.data.model.Scene;
+import com.threes.scenespotinseoul.ui.gallery.*;
+import com.threes.scenespotinseoul.ui.scene.SceneDetailActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.threes.scenespotinseoul.utilities.ConstantsKt.EXTRA_SCENE_ID;
+
+public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder>{
+    private List<Scene> captured;
+
+    GalleryAdapter(List<Scene> captured) {
+        this.captured = captured;
+
+    }
+    public GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view =
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_gallery_recyclerview, parent, false);
+        return new GalleryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GalleryViewHolder holder, int position) {
+        Context context = holder.itemView.getContext();
+        Log.v("캡쳐된디 이미지",captured.get(position).getCapturedImage());
+        RequestOptions requestOptions = new RequestOptions().centerCrop();
+        if(captured!=null){
+            Glide.with(context)
+                    .load(Uri.parse(captured.get(position).getCapturedImage()))
+                    .apply(requestOptions)
+                    .into(holder.image);
+
+        }
+
+        holder.itemView.setOnClickListener(
+                v -> {
+                    // 명장면 상세 액티비티로 장면 아이디 값 넘김
+                    Intent intent = new Intent(context, SceneDetailActivity.class);
+                    intent.putExtra(EXTRA_SCENE_ID, captured.get(position).getId());
+                    context.startActivity(intent);
+                });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(captured !=null){
+            return captured.size();
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public class GalleryViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        public GalleryViewHolder (View itemView){
+            super(itemView);
+            image = itemView.findViewById(R.id.gallery_recycler_image);
+        }
+
+    }
+
+}
+
--- app/src/main/java/com/threes/scenespotinseoul/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/threes/scenespotinseoul/ui/gallery/GalleryFragment.java
@@ -1,23 +1,104 @@
 package com.threes.scenespotinseoul.ui.gallery;
 
+
+import static com.threes.scenespotinseoul.utilities.AppExecutorsHelperKt.runOnDiskIO;
+import static com.threes.scenespotinseoul.utilities.AppExecutorsHelperKt.runOnMain;
+
+import android.content.ClipData;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.threes.scenespotinseoul.R;
+import com.threes.scenespotinseoul.data.AppDatabase;
+import com.threes.scenespotinseoul.data.model.Scene;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GalleryFragment extends Fragment {
+    RecyclerView recyclerView;
+    GalleryAdapter adapter;
+    GridLayoutManager layoutManager;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
 
-  @Nullable
-  @Override
-  public View onCreateView(
-      @NonNull LayoutInflater inflater,
-      @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
-    View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
-    return rootView;
-  }
+
+
+//        TextView title = (TextView)view.findViewById(R.id.gtitle);
+//        View line = (View)view.findViewById(R.id.gline);
+        recyclerView = (RecyclerView)view.findViewById(R.id.recycler);
+
+        // 미디어 데이터 가져옴 Executor 사용
+        AppDatabase db = AppDatabase.getInstance(getContext());
+        db.sceneDao()
+                .loadAllAreCapturedWithLive()
+                .observe(
+                        this,
+                        scene -> {
+                            for(int i=0;i<scene.size();i++){
+
+                                if (scene.get(i) != null) {
+
+                                    adapter = new GalleryAdapter(scene);
+                                    recyclerView.setAdapter(adapter);
+                                    recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+                                    recyclerView.setHasFixedSize(true);
+
+
+                                }
+                            }
+
+                        });
+
+
+
+
+
+
+//        runOnDiskIO(
+//                () -> {
+//                    // List<Media> media = db.mediaDao().loadAll();
+//
+//                    List<Scene> scenes = db.sceneDao().loadAll();
+//                    List<Scene> capturedScene = new ArrayList<>();
+//                    int s=0;
+//                    for (int i = 0; i < scenes.size(); i++) {
+//
+//                        if(scenes.get(i).isCaptured() == true) {
+//                            s++;
+//                            capturedScene.add(scenes.get(i));
+//                        }
+//                    }
+//                    Log.v("캡쳐된 이미지수",""+s);
+//
+//                    runOnMain(
+//                            () -> {
+//                                if(capturedScene!=null){
+//                                    adapter = new GalleryAdapter(capturedScene);
+//                                    recyclerView.setAdapter(adapter);
+//                                    recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+//                                    recyclerView.setHasFixedSize(true);
+//
+//                                }
+//                    });
+//        });
+        return view;
+    }
+
+
+
+
+
 }
