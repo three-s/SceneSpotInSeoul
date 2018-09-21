543845f45bbf7243f0fb9b35d214c09cd90cdcde
NamHyun Gu <mnhan0403@gmail.com>
2018-09-21 17:34:35 +0900

    PictureActivity Background 색상 변경 및 뒤로가기 버튼 추가

--- app/src/main/java/com/threes/scenespotinseoul/ui/scene/PictureActivity.java
+++ app/src/main/java/com/threes/scenespotinseoul/ui/scene/PictureActivity.java
@@ -1,59 +1,71 @@
 package com.threes.scenespotinseoul.ui.scene;
 
+import static com.threes.scenespotinseoul.utilities.ConstantsKt.EXTRA_SCENE_ID;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ImageView;
-
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
-
 import com.threes.scenespotinseoul.R;
 import com.threes.scenespotinseoul.data.AppDatabase;
 
-import static com.threes.scenespotinseoul.utilities.ConstantsKt.EXTRA_SCENE_ID;
-
 public class PictureActivity extends AppCompatActivity {
-    private ImageView picture;
-    String SceneId;
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_picture);
 
+  String SceneId;
+  private ImageView picture;
 
-        picture = findViewById(R.id.into_picture);
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_picture);
 
-        Intent intent = getIntent();
-        //Log.e("받은intent", intent.hasExtra(EX)+"");
-        if (intent != null && intent.hasExtra(EXTRA_SCENE_ID)) {
-            SceneId = intent.getStringExtra(EXTRA_SCENE_ID);
+    picture = findViewById(R.id.into_picture);
 
-            if (SceneId ==null) {
-                Log.e("PictureActivity", "Can't receive scene id");
-                finish();
-            }
-            AppDatabase db = AppDatabase.getInstance(this);
-            db.sceneDao()
-                    .loadByIdWithLive(SceneId)
-                    .observe(this,
-                            scene -> {
-                                if (scene != null) {
-                                    Log.v("URI",scene.getUploadedImage());
-                                    Glide.with(this)
-                                            .load(Uri.parse(scene.getUploadedImage()))
-                                            .apply(new RequestOptions().fitCenter())
-                                            .into(picture);
-
-                                }
-
-
-                            });
-        }
-
-
+    ActionBar supportActionBar = getSupportActionBar();
+    if (supportActionBar != null) {
+      supportActionBar.setDisplayHomeAsUpEnabled(true);
+      supportActionBar.setHomeButtonEnabled(true);
+      supportActionBar.setTitle("");
     }
+
+    Intent intent = getIntent();
+    //Log.e("받은intent", intent.hasExtra(EX)+"");
+    if (intent != null && intent.hasExtra(EXTRA_SCENE_ID)) {
+      SceneId = intent.getStringExtra(EXTRA_SCENE_ID);
+
+      if (SceneId == null) {
+        Log.e("PictureActivity", "Can't receive scene id");
+        finish();
+      }
+      AppDatabase db = AppDatabase.getInstance(this);
+      db.sceneDao()
+          .loadByIdWithLive(SceneId)
+          .observe(this,
+              scene -> {
+                if (scene != null) {
+                  Log.v("URI", scene.getUploadedImage());
+                  Glide.with(this)
+                      .load(Uri.parse(scene.getUploadedImage()))
+                      .apply(new RequestOptions().fitCenter())
+                      .into(picture);
+
+                }
+              });
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == android.R.id.home) {
+      finish();
+    }
+    return super.onOptionsItemSelected(item);
+  }
 }
