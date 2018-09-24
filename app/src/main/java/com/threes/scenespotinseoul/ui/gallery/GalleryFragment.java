bee43534911b6267bb65c15b4bde31c61eb03700
NamHyun Gu <mnhan0403@gmail.com>
2018-09-24 10:17:47 +0900

    갤러리 화면에 사진이 없을 경우에 표시할 메세지 추가

--- app/src/main/java/com/threes/scenespotinseoul/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/threes/scenespotinseoul/ui/gallery/GalleryFragment.java
@@ -9,12 +9,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import com.threes.scenespotinseoul.R;
 import com.threes.scenespotinseoul.data.AppDatabase;
 import java.util.Objects;
 
 public class GalleryFragment extends Fragment {
 
+  TextView mTvNoPictures;
   RecyclerView recyclerView;
   GalleryAdapter adapter;
 
@@ -22,6 +24,7 @@ public class GalleryFragment extends Fragment {
   public View onCreateView(
       @NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+    mTvNoPictures = view.findViewById(R.id.tv_no_pictures);
     recyclerView = view.findViewById(R.id.recycler);
     AppDatabase db = AppDatabase.getInstance(Objects.requireNonNull(getContext()));
     db.sceneDao()
@@ -29,10 +32,17 @@ public class GalleryFragment extends Fragment {
         .observe(
             this,
             scene -> {
-              adapter = new GalleryAdapter(getActivity(), scene);
-              recyclerView.setAdapter(adapter);
-              recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
-              recyclerView.setHasFixedSize(true);
+              if (scene != null && !scene.isEmpty()) {
+                adapter = new GalleryAdapter(getActivity(), scene);
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+                recyclerView.setHasFixedSize(true);
+                mTvNoPictures.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+              } else {
+                mTvNoPictures.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+              }
             });
     return view;
   }
